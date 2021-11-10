50b06fa18f9906bd5b0f36f4170b0b52ad06a7bc
tesla <kela256@gmail.com>
2021-11-10 18:16:08 +0200

    change formating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,35 +4,29 @@ import { NavigationEnd, Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
   public param: string;
-  public state:string;
-  public currentUrl:string;
+  public state: string;
+  public currentUrl: string;
 
-
-  constructor(private router: Router){
+  constructor(private router: Router) {
     this.currentUrl = window.location.href;
 
     router.events.subscribe((event) => {
-
-      if(event instanceof NavigationEnd) { 
+      if (event instanceof NavigationEnd) {
         this.currentUrl = window.location.href;
       }
-  });
+    });
   }
 
-  public addUrlInBrowserHistory():void{
+  public addUrlInBrowserHistory(): void {
     history.pushState({}, this.param, `?param=${this.param}`);
     this.currentUrl = window.location.href;
   }
 
-  public goBack():void{
-    history.back();    
+  public goBack(): void {
+    history.back();
   }
-
-
-
 }
